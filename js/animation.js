0c454f3ac7c4eadeba894899e58e9445ea9a4a94
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2022-11-17 21:52:51 -0700

    java script

---
+++ js/animation.js
@@ -0,0 +1,8 @@
+// All class decleration section
+
+let workingcontainer = document.querySelector(".work-img-container");
+let worknavli = document.querySelector(".work-nav-li");
+
+// variable decleration section
+
+let workitems ,worksectionimg
